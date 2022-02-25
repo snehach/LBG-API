23164e828c3edab709bbfbbe148d4e7d9b098cc4
snehach <snehac.174@gmail.com>
2022-02-25 14:36:15 +0000

    added more test features

--- app.test.js
+++ app.test.js
@@ -6,47 +6,73 @@ const build = require('./app').itemBuilder;
 describe('Unit Tests', () => {
 
     test('item object builder', () => {
-        expect(build('my first thing', 'a test item', 4, 1))
+        expect(build('my first item', 'a test item', 4, 1))
         .toMatchObject(
             {
-                name : "my first thing",
-                description : "a test item",
+                name : 'my first item',
+                description : 'a test item',
                 price : 4,
-                _id : 2
+                _id : 1
             }
         );
     });
 
 });
 
-describe('GET requests', () => {
+describe('CREATE request', () => {
+    
+    test('CREATE item test, expect 201', async () => {
+        const res = await request(app)
+                            .post('/create')
+                            .send({
+                                name : "test item",
+                                description : "test description",
+                                price : 99
+                            });
+        expect(res.statusCode).toBe(201);
+    });
+
+});
+
+describe('READ requests', () => {
     
     test('GET /read endpoint, expect 200', async () => {
         const res = await request(app).get('/read')
         expect(res.statusCode).toBe(200);
     });
 
-    // time to create a bad endpoint test (404)
-    //endpoints which does not exist
     test('GET /bad endpoint, expect 404', async () => {
         const res = await request(app).get('/bad')
+        expect(res.statusCode).toBe(404);
+    });
+    
+    test('GET /read/1 endpoint, expect 200', async () => {
+        const res = await request(app).get('/read/1')
         expect(res.statusCode).toBe(200);
     });
 
 });
 
-describe('CREATE request', () => {
+describe('UPDATE request', () => {
     
-    // we could also test the create request
-    test(`CREATE item test, expect 201`, async () => {
+    test('UPDATE item test, expect 200', async () => {
         const res = await request(app)
-                            .post('/create')
+                            .put('/update/1')
                             .send({
-                                name : "test item", 
-                                description : "test desc",
-                                price : 99
-                            })
-        expect(res.statusCode).toBe(201);
-    })
+                                name : "updated item",
+                                description : "updated description",
+                                price : 0
+                            });
+        expect(res.statusCode).toBe(200);
+    });
+
+});
+
+describe('DELETE requests', () => {
+    
+    test('DELETE /delete/1 endpoint, expect 200', async () => {
+        const res = await request(app).delete('/delete/1')
+        expect(res.statusCode).toBe(200);
+    });
 
 });
