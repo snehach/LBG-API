bf3f3819eb30197e0745cc082c08b2de6efe2221
sneha.chaudhary <snehac.174@gmail.com>
2022-03-10 13:29:07 +0000

    SC: trtying to automate build image

--- buildnodeappprj2.sh
+++ buildnodeappprj2.sh
@@ -2,6 +2,11 @@
 
 cd /home/sneha_chaudhary/LBG_API/LBG-API
 
+#check if image exists, then remove image to recreate
+#if [ docker ps -a | grep nodeappcon > /dev/null ]; then
+#	OLD_IMAGE=$(docker ps -a | grep CONTAINER | awk '{print $2}')
+#fi
+
 docker build -t nodeappprj2:latest .
 
 
